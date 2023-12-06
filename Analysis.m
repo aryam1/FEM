929cd4297abd65eedbdca18fe545fc66f4841cde
aryam1 <arya.mukherjee@hotmail.com>
2023-12-06 00:17:16 +0000

    added gaussian quadrature

--- Analysis.m
+++ Analysis.m
@@ -4,15 +4,14 @@ x=linspace(0,1,1+1/dx);
 t=linspace(0,tmax,1+tmax/dt);
 
 tran = zeros(1+1/dx,1+tmax/dt);
-num = zeros(1+1/dx,1+tmax/dt);
+num = Run(dx,dt,tmax,theta);
 
-
-for i = 1:1/dx
-    for j = 1:tmax/dt
+for j = 1:tmax/dt+1
+    for i = 1:1/dx+1
         tran(i,j) = TransientAnalyticSoln(x(i),t(j));
+        E(i) = tran(i,j) - num(i,j);
     end
 end
 
-num = Run(dx,dt,tmax,theta);
 err = immse(tran,num)
 end
