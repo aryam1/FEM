0d417a1fd569546a77d0c585a059fda7d7ee9692
aryam1 <arya.mukherjee@hotmail.com>
2023-12-10 02:17:18 +0000

    Adjusted behaviour of objects and class methods to reduce calls

--- MeshObj.m
+++ MeshObj.m
@@ -10,51 +10,169 @@ classdef MeshObj
         rBoundary
         rBoundaryType
         basisType
-        Dvec (2,:) double % spatially varying diffusion term 
+        Dvec (2,:) double % spatially varying diffusion term
         Lvec (2,:) double % spatially varying reaction term
-        Fvec (1,:) double % spatially varying source term
+        Fvec (2,:) double % spatially varying source term
+        NBc
         elems = ElemObj.empty
+        nVec % Position of every global node
+        transient = false
+        KGlobal
+        MGlobal
+        FGlobal
+        solution
     end
     properties (Dependent)
-        dx % Spatial width of linear nodes
-        L2 % L2 Norm error for the mesh
         globalN % Number of nodes in mesh
-        nVec % Position of every global node
     end
     methods
-        function obj = MeshObj(minX, maxX, elemN, boundaries, basis, t)
-             obj.minX = minX;
-             obj.maxX = maxX;
-             obj.elemN = elemN;
-             
-             [obj.lBoundary,obj.lBoundaryType,obj.rBoundary,obj.rBoundaryType] = boundaries{:};
-             obj.basisType = basis;
-             obj.t = t;
-             obj = obj.elemSetup();
+        function obj = MeshObj(minX, maxX, elemN, boundaries, basis, t, dt)
+            obj.minX = minX;
+            obj.maxX = maxX;
+            obj.elemN = elemN;
+            obj.nVec = linspace(minX,maxX,(elemN*basis)+1);
+
+            [obj.lBoundary,obj.lBoundaryType,obj.rBoundary,obj.rBoundaryType] = boundaries{:};
+            obj.NBc = zeros(obj.globalN,1);
+            obj.NBc(1) = obj.lBoundary * (obj.lBoundaryType == "n");
+            obj.NBc(end) = obj.rBoundary * (obj.rBoundaryType == "n");
+
+            obj.basisType = basis;
+            obj.t = t;
+            obj.dt = dt;
         end
 
-        function obj = elemSetup(obj)
-            for i = 1:obj.elemN
-                obj.elems(i) = ElemObj(i,obj.nVec(i),obj.nVec(i+1),obj.basisType);
+        function self = ElemSetup(self)
+            for i = 1:self.elemN
+                I = self.basisType * (i-1) + 1;
+                self.elems(i) = ElemObj(i,self.nVec(I:I+self.basisType),self.basisType);
             end
         end
 
-        function xStep = get.dx(obj)
-            xStep = (obj.maxX - obj.minX)/obj.elemN; % Computing linear spatial step
+        function self = SetParams(self,dVec,lVec,fVec)
+            self = self.ElemSetup();
+
+            dPointer = 1;
+            lPointer = 1;
+            fPointer = 1;
+            for i = 1:self.elemN
+                for j = 1:self.basisType+1
+                    while self.elems(i).x(j) > dVec(1,dPointer)
+                        dPointer = dPointer + 1;
+                    end
+                    self.elems(i).D(j) = dVec(2,dPointer);
+
+                    while self.elems(i).x(j) > lVec(1,lPointer)
+                        lPointer = lPointer + 1;
+                    end
+                    self.elems(i).L(j) = lVec(2,lPointer);
+
+                    while self.elems(i).x(j) > fVec(1,fPointer)
+                        fPointer = fPointer + 1;
+                    end
+                    self.elems(i).F(j) = fVec(2,fPointer);
+                end
+            end
         end
-        
-        
-        function gNodes = get.nVec(obj)
-            gNodes = obj.minX:obj.dx:obj.maxX; % Change to Array of element objects
+
+        function self = GlobalSetup(self)
+            KG = zeros(self.globalN);
+            MG = zeros(self.globalN);
+            FG = zeros(self.globalN,1);
+
+            [psi,dpsi] = self.BasisFunc(self.basisType);
+
+            for i = 1:self.elemN
+                [K,M,F] = self.elems(i).LocalMatrix(psi,dpsi,4);
+                I = self.basisType*(i-1)+1; %adjusting the insertion index for all basis type
+                I2 = I+self.basisType;
+                % Insert local matrices into global matrices
+                KG(I:I2,I:I2) = KG(I:I2,I:I2) + K;
+                MG(I:I2,I:I2) = MG(I:I2,I:I2) + M;
+
+                FG(I:I2,1) = FG(I:I2,1) + F;
+            end
+            self.KGlobal = KG;
+            self.MGlobal = MG;
+            self.FGlobal = FG;
         end
-        
-        
-        function error = get.L2(obj)
-            error =1; % Change to sum e of each element
+
+        function self = Solve(self,theta,previous)
+            if ~self.transient
+                self.KGlobal = previous.KGlobal;
+                self.MGlobal = previous.MGlobal;
+                self.FGlobal = previous.FGlobal;
+                self.elems = previous.elems;
+            else
+                self = self.GlobalSetup();
+            end
+            res = (self.MGlobal-(1-theta).*self.dt.*self.KGlobal)*previous.solution;
+            res = res + self.dt*theta*(self.FGlobal + self.NBc);
+            res = res + self.dt*(1-theta)*(previous.FGlobal + previous.NBc);
+
+            gm = self.MGlobal+(theta.*self.dt.*self.KGlobal);
+
+            if self.lBoundaryType == "d"
+                gm(1,:) = [1,zeros(1,self.globalN-1)];
+                res(1)=self.lBoundary;
+            end
+            if self.rBoundaryType == "d"
+                gm(end,:) = [zeros(1,self.globalN-1),1];
+                res(end)=self.rBoundary;
+            end
+
+            self.solution = gm\res;
+
         end
+
         function nodes = get.globalN(obj)
             nodes = (obj.elemN*obj.basisType)+1; % Number of nodes is 1 greater than elements times basis order
         end
-        
+
+
+
     end
+
+    methods (Static)
+        function [psi,dpsi] = BasisFunc(basis)
+            funcs = {
+                {@(x)(1-x)/2, @(x)(1+x)/2} ...
+                {@(x)-0.5,@(x)0.5};
+                {@(x)0.5.*x.*(x-1), @(x)1-x.^2, @(x)0.5.*x.*(1+x)} ...
+                {@(x)x-0.5,@(x)-2*x,@(x)x+0.5}
+                };
+            try
+                psi = funcs{basis};
+                dpsi = funcs{basis+2};
+            catch
+                causeException = MException('MeshObj:BasisFunc',"Invalid basis function order");
+                throw(causeException);
+            end
+        end
+
+        function [p,w] = GQScheme(order)
+            data = {
+                [0; 2]
+                [-1/sqrt(3) 1/sqrt(3); 1 1]
+                [-sqrt(3/5) 0 sqrt(3/5); 5/9 8/9 5/9]
+                [-sqrt(3/7 + (2/7*sqrt(1.2))) ...
+                -sqrt(3/7 - (2/7*sqrt(1.2))) ...
+                sqrt(3/7 - (2/7*sqrt(1.2))) ...
+                sqrt(3/7 + (2/7*sqrt(1.2)));
+                (18-sqrt(30))/36 (18+sqrt(30))/36 ...
+                (18+sqrt(30))/36 (18-sqrt(30))/36]
+                };
+
+            try
+                gq = data{order};
+                p= gq(1,:);
+                w= gq(2,:);
+            catch
+                causeException = MException('MeshObj:GQ',"Invalid quadrature order");
+                throw(causeException);
+            end
+        end
+
+    end
+
 end
--- OORun.m
+++ OORun.m
@@ -1,10 +1,50 @@
+basis = 1;
+
 x0 = 0;
 x1 = 1;
-dx = 0.1;
+dx = 0.01;
+Nx = 1+(basis * x1/dx);
 
 t0 = 0;
 t1 = 2;
 dt = 0.01;
+Nt = 1+t1/dt;
 
 xvals = x0:dx:x1;
 tvals = t0:dt:t1;
+
+%matrix = MeshObj.empty(0,Nt);
+
+IC = 0;
+LB = 0;
+RB = 1;
+
+sol = zeros(Nx,Nt);
+sol(:,1) = IC;
+
+
+% for i = 1:t1/dt + 1
+%     matrix(i) = MeshObj(x0,x1,x1/dx,{LB,'d',RB,'d'},basis,tvals(i),dt);
+%     if i == 1
+%         matrix(i) = matrix(i).SetParams([1;1],[1;0],[1;0]);
+%         matrix(i).solution = sol(:,1);
+%         matrix(i) = matrix(i).GlobalSetup();
+%         continue
+%     end
+%     matrix(i) = matrix(i).Solve(0.5,matrix(i-1));
+% end
+
+previous = MeshObj.empty;
+for i = 1:t1/dt + 1
+    mesh = MeshObj(x0,x1,x1/dx,{LB,'d',RB,'d'},basis,tvals(i),dt);
+    if i == 1
+        mesh = mesh.SetParams([1;1],[1;0],[1;0]);
+        mesh.solution = sol(:,1);
+        mesh = mesh.GlobalSetup();
+        previous = mesh;
+        continue
+    end
+    mesh = mesh.Solve(0.5,previous);
+    previous = mesh;
+    sol(:,i) = mesh.solution;
+end
